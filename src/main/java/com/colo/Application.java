d5a3be69bc1132ed22ce442b366453038912143c
Denis Řeháček <denis.rehace@etnetera.cz>
2017-11-21 16:33:52 +0100

    Fix DB config

--- src/main/java/com/colo/Application.java
+++ src/main/java/com/colo/Application.java
@@ -18,7 +18,7 @@ import java.util.Arrays;
  */
 @SpringBootApplication
 @EnableAutoConfiguration
-@EntityScan//( basePackages = {"com.colo.data"} )
+@EntityScan
 public class Application extends SpringBootServletInitializer {
 
     @Override
@@ -26,39 +26,8 @@ public class Application extends SpringBootServletInitializer {
         return application.sources(Application.class);
     }
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
-        String driver = "com.mysql.jdbc.Driver";
-        Class.forName(driver).newInstance();
-        String protocol = "jdbc:mysql:";
-        Connection conn = DriverManager.getConnection(protocol + "//localhost:3306/world","root","root");
-        init(conn);
-        Statement stmt = conn.createStatement();
-        String query = "SELECT ID, NAME FROM FIRSTTABLE";
-        ResultSet rs = stmt.executeQuery(query);
-        while (rs.next()) {
-            System.out.println("ID" + rs.getInt("ID") + " NAME: " + rs.getString("NAME") );
-        }
-        stmt.close();
-        conn.close();
-
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    private static void init(Connection conn) throws SQLException {
-        DatabaseMetaData dbmd = conn.getMetaData();
-        ResultSet rs = dbmd.getTables(null, null, "FIRSTTABLE", null);
-        if (rs.next()) {
-            System.out.println("Table " +  rs.getString(3) + " exists");
-        } else {
-            Statement stmt = conn.createStatement();
-            String query = "CREATE TABLE FIRSTTABLE (ID INT PRIMARY KEY, NAME VARCHAR(12))";
-            stmt.executeUpdate(query);
-            stmt.close();
-            stmt = conn.createStatement();
-            query = "INSERT INTO FIRSTTABLE VALUES (10,'TEN'),(20,'TWENTY'),(30,'THIRTY')";
-            stmt.executeUpdate(query);
-            stmt.close();
-        }
     }
 
     @Bean
